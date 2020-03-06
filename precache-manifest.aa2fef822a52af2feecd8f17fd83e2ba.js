edf115d27d19272addac0028ae89bb736f5dc7eb
Tri Agus Setiawan <ta.setiawan18@gmail.com>
2020-03-06 14:50:01 +0700

    deploy

---
+++ precache-manifest.aa2fef822a52af2feecd8f17fd83e2ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711b92feb56eda5e14a2b2f31e688536",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c28fa8f17fad3d74bee",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c3df7853c3c2bca3f43",
+    "url": "/static/js/2.9867e100.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.9867e100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c28fa8f17fad3d74bee",
+    "url": "/static/js/main.433f7b3d.chunk.js"
+  },
+  {
+    "revision": "a1eba35b73d7f5d2c482",
+    "url": "/static/js/runtime-main.ee367bc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
